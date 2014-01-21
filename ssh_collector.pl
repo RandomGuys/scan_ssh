0782106ed8f153d75b7e766c73f8970fabbd011e
Pascal <edouard.jp.pascal@gmail.com>
2014-01-21 14:00:32 +0100

    Ajout des Threads + tests d'erreur [8665707] [8665715] [8665723]

--- ssh_collector.pl
+++ ssh_collector.pl
@@ -2,38 +2,91 @@
 
 use Term::ANSIColor qw(:constants);
 use POSIX 'strftime';
+use threads;
+use Thread::Semaphore;
 
-my $start = time;
-
-open (ADDR, $ARGV[0]);
+open (ADDR, $ARGV[0])
+        or die "Usage : perl ssh_collector.pl adresses\n";
 
 unless ( -d "keys") {
-	system "mkdir keys";
+        system "mkdir keys";
+}
+unless ( -d "bannieres") {
+        system "mkdir bannieres";
+}
+
+my $start               = time;
+my $semaphore_no        = 5;
+my $sema                = Thread::Semaphore->new($semaphore_no);
+my $info_rep            = "bannieres";
+my $keys_rep            = "keys";
+
+# SUBROUTINES
+sub getKey {
+        $sema->down();
+        # Get the thread id. Allows each thread to be identified.
+        my ($IP) = @_;
+        $id = threads->tid();
+
+        # [DEBUG]
+        # print "Thread $id : Fetching $IP ...\n";
+        system "rm -rf tmp_$IP.pub; ssh-keyscan -t rsa $IP 2> $info_rep/$IP.info | sed '/^#/d' | cut -d' ' -f2,3 > tmp_$IP.pub 2> /dev/null";
+
+        if ($? != 0) {
+                print RED, "Something wrong happened with $IP\n",RESET;
+                system "rm -rf $info_rep/$IP.info";
+                $sema->up();
+                return;
+        };
+
+        # Get size of file
+        $size_of_file = `ls -sh tmp_$IP.pub | cut -d' ' -f1 2> /dev/null`;
+
+        # Check if file is empty
+        if ( $size_of_file == 0 ) {
+                print RED, "$IP : No RSA key found...\n",RESET;
+                system "rm -rf $info_rep/$IP.info";
+                system "rm -rf tmp_$IP.pub";
+                $sema->up();
+        } else {
+
+                print "$IP : Storing key...\n";
+                system "chmod 0600 tmp_$IP.pub; ssh-keygen -e -f tmp_$IP.pub -m PEM -P '' > $keys_rep/$IP.pem";
+                system "rm -rf tmp_$IP.pub";
+                print GREEN,"$IP : done!\n",RESET;
+
+                # [DEBUG]
+                # print "Thread $id done!\n";
+                $sema->up();
+        };
 }
 
 while (<ADDR>) {
         chomp;
-				$duration = time - $start;
-				($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime $duration;
-				$hour = $hour - 1;
-        print "[$hour:$min:$sec] Fetching $_...\n";
-        system "rm -rf tmp.pub; ssh-keyscan -t rsa $_ | sed '/^#/d' | cut -d' ' -f2,3 > tmp.pub 2> /dev/null";
-        if ($? != 0) {
-                print RED, "Something wrong happened with $_\n",RESET;
-                next;
-        }
-				if ( -z 'tmp.pub' ) {
-					print RED, "No RSA key found...\n",RESET;
-					next;
-				}
-        print "Storing key...\n";
-        system "chmod 0600 tmp.pub; ssh-keygen -e -f tmp.pub -m PEM -P '' > keys/$_.pem";
-        print GREEN,"$_ done!\n",RESET;
+
+        my $duration = time - $start;
+        my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime $duration;
+        my $hour = $hour - 1;
+
+        $line = $_;
+
+        # Starting
+        print "[$hour:$min:$sec] Fetching $line...\n";
+
+        # Create threads wirth IP
+        $th = threads->create(\&getKey, $line);
 }
 
-if ( -e "tmp.pub" ) {
-	system "rm -rf tmp.pub";
+# Join remaining Threads before closing
+foreach my $thr (threads->list()){
+        $id = $thr->tid();
+        # [DEBUG]
+        # print "joining : thread $id\n";
+        $thr->join();
 }
 
-close (ADDR);
+print "Cleaning...\n";
+system "rm -rf tmp*";
 
+close (ADDR)
+        or die "impossible de fermer le fichier : $!\n";
